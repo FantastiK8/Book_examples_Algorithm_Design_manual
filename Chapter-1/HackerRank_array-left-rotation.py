241eada261b088af47759a873349b42e058c1bf1
FantastiK8 <katerina.montoban@gmail.com>
2021-11-24 21:34:24 +0000

    HackerRank array left rotation solved EASY.

--- Chapter-1/HackerRank_array-left-rotation.py
+++ Chapter-1/HackerRank_array-left-rotation.py
@@ -1,7 +1,26 @@
 #https://www.hackerrank.com/challenges/array-left-rotation/problem?isFullScreen=true
 
 # A left rotation operation on an array of size n shifts each of the array's elements 1 
-# unit to the left. Given an integer, d, rotate the array that many steps left and return the result.
+# unit to the left. Given an integer, d, rotate the array that many steps left and return 
+# the result.
+
+"""
+Example:
+d = 2
+arr = [1,2,3,4,5]
+after 2 rottations
+arr' = [3,4,5,1,2]
+
+
+solutions:
+-loop in the range of d
+-pop last item and insert at the beginning
+
+for element in range(d):
+    last_element = arr.pop()
+    arr.insert(0, last_element)
+
+"""
 
 #!/bin/python3
 
@@ -22,21 +41,30 @@ import sys
 
 def rotateLeft(d, arr):
     # Write your code here
+    for element in range(d):
+        first_element = arr.pop(0)
+        arr.append(first_element)
+    return arr
 
-    if __name__ == '__main__':
-        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-        first_multiple_input = input().rstrip().split()
+#     first_multiple_input = input().rstrip().split()
 
-        n = int(first_multiple_input[0])
+#     n = int(first_multiple_input[0])
 
-        d = int(first_multiple_input[1])
+#     d = int(first_multiple_input[1])
 
-        arr = list(map(int, input().rstrip().split()))
+#     arr = list(map(int, input().rstrip().split()))
 
-        result = rotateLeft(d, arr)
+#     result = rotateLeft(d, arr)
 
-        fptr.write(' '.join(map(str, result)))
-        fptr.write('\n')
+#     fptr.write(' '.join(map(str, result)))
+#     fptr.write('\n')
 
-        fptr.close()
+#     fptr.close()
+
+
+d = 2
+arr = [1,2,3,4,5]
+print(rotateLeft(d, arr))
